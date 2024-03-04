64402f879d7be0c5b7f23e04eb57b1abf653e8d0
Rosemilson <rozbr96@gmail.com>
2024-03-04 09:24:06 -0300

    Improve data seeding performance

--- src/import_from_csv.rb
+++ src/import_from_csv.rb
@@ -4,6 +4,11 @@ require_relative 'models'
 
 
 rows = CSV.read './sample/data.csv', col_sep: ';'
+cache = {
+  :patients => {},
+  :doctors => {},
+  :exam_types => {},
+}
 
 columns = rows.shift
 
@@ -27,14 +32,20 @@ rows.each do |row|
 
   exam_type_data = { name: data['tipo exame'], limits: data['limites tipo exame'] }
 
-  patient = Patient.get_or_create patient_data
+  patient_data_key = patient_data.values.join
+  patient = cache[:patients][patient_data_key] || Patient.get_or_create(patient_data)
   puts "Patient: #{patient['id']}"
+  cache[:patients][patient_data_key] = patient
 
-  doctor = Doctor.get_or_create doctor_data
+  doctor_data_key = doctor_data.values.join
+  doctor = cache[:doctors][doctor_data_key] || Doctor.get_or_create(doctor_data)
   puts "Doctor: #{doctor['id']}"
+  cache[:doctors][doctor_data_key] = doctor
 
-  exam_type = ExamType.get_or_create exam_type_data
+  exam_type_data_key = exam_type_data.values.join
+  exam_type = cache[:exam_types][exam_type_data_key] || ExamType.get_or_create(exam_type_data)
   puts "Exam Type: #{exam_type['id']}"
+  cache[:exam_types][exam_type_data_key] = exam_type
 
   exam_data = {
     patient_id: patient['id'], doctor_id: doctor['id'], exam_type_id: exam_type['id'],
