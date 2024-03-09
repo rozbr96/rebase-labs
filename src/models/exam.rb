94bbbc0734500eb1ae7da5dfeee29d87e2f4fff8
Rosemilson <rozbr96@gmail.com>
2024-03-09 14:07:21 -0300

    Refact tests v2 data building

--- src/models/exam.rb
+++ src/models/exam.rb
@@ -6,4 +6,70 @@ class Exam < Model
 
   TABLE_NAME = 'exam'
   TABLE_COLUMNS = %i[patient_id doctor_id exam_type_id date result token_result]
+
+  def self.select joins:, fields_selection:, version: 'V1'
+    v1_data = super(joins:, fields_selection:)
+
+    return v1_data if version == 'V1'
+
+    v2_data v1_data
+  end
+
+  private
+
+  def self.v2_data rows
+    dates = {}
+    doctors = {}
+    patients = {}
+    tokens = []
+    tests = Hash.new { |hash, key| hash[key] = [] }
+
+    rows.each do |row_data|
+      token = row_data['exam_token_result']
+      patients[token] = get_patient_data row_data
+      doctors[token] = get_doctor_data row_data
+      tests[token] << get_test_data(row_data)
+      dates[token] = row_data['exam_date']
+      tokens << token
+    end
+
+    tokens.uniq.map do |token|
+      {
+        :patient => patients[token],
+        :doctor => doctors[token],
+        :tests => tests[token],
+        :date => dates[token],
+        :result_token => token
+      }
+    end
+  end
+
+  def self.get_doctor_data row_data
+    {
+      :name => row_data['doctor_name'],
+      :email => row_data['doctor_email'],
+      :crm => row_data['doctor_crm'],
+      :state => row_data['doctor_state']
+    }
+  end
+
+  def self.get_patient_data row_data
+    {
+      :citizen_id_number => row_data['patient_citizen_id_number'],
+      :name => row_data['patient_name'],
+      :email => row_data['patient_email'],
+      :birth_date => row_data['patient_birth_date'],
+      :street_address => row_data['patient_street_address'],
+      :city => row_data['patient_city'],
+      :state => row_data['patient_state']
+    }
+  end
+
+  def self.get_test_data row_data
+    {
+      :type => row_data['exam_type_name'],
+      :result => row_data['exam_result'],
+      :limits => row_data['exam_type_limits']
+    }
+  end
 end
--- src/server/controller/api/v2/controller.rb
+++ src/server/controller/api/v2/controller.rb
@@ -12,53 +12,8 @@ module Controller
             Doctor => %w[name email crm state],
             Patient => %w[citizen_id_number name email birth_date street_address city state],
             ExamType => %w[name limits],
-          }
+          }, version: 'V2'
         )
-
-        dates = {}
-        doctors = {}
-        patients = {}
-        tokens = []
-        tests = Hash.new { |hash, key| hash[key] = [] }
-
-        exams.each do |exam|
-          patients[exam['exam_token_result']] ||= {
-            :citizen_id_number => exam['patient_citizen_id_number'],
-            :name => exam['patient_name'],
-            :email => exam['patient_email'],
-            :birth_date => exam['patient_birth_date'],
-            :street_address => exam['patient_street_address'],
-            :city => exam['patient_city'],
-            :state => exam['patient_state']
-          }
-
-          doctors[exam['exam_token_result']] ||= {
-            :name => exam['doctor_name'],
-            :email => exam['doctor_email'],
-            :crm => exam['doctor_crm'],
-            :state => exam['doctor_state']
-          }
-
-          dates[exam['exam_token_result']] ||= exam['exam_date']
-
-          tests[exam['exam_token_result']] << {
-            :type => exam['exam_type_name'],
-            :result => exam['exam_result'],
-            :limits => exam['exam_type_limits']
-          }
-
-          tokens << exam['exam_token_result']
-        end
-
-        exams = tokens.uniq.map do |token|
-          {
-            :patient => patients[token],
-            :doctor => doctors[token],
-            :tests => tests[token],
-            :date => dates[token],
-            :result_token => token
-          }
-        end
 
         response.json data: exams, status: :ok
       end
