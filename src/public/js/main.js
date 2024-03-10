e2d0e63935a295b35a2135ef23431f624b21d613
Rosemilson <rozbr96@gmail.com>
2024-03-10 05:44:48 -0300

    Adapt home page to use endpoint version V2

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,17 +1,44 @@
 
 const formatDate = (date) => date.split('-').reverse().join('/')
 
+const doctorFields = ['name', 'crm', 'email']
+const patientFields = ['name', 'citizen_id_number', 'birth_date']
+const testFields = ['type', 'limits', 'result']
+
 const fieldsData = {
-  'patient_name': { align: 'left', format: null },
-  'patient_citizen_id_number': { align: 'center', format: null },
-  'patient_birth_date': { align: 'center', format: formatDate },
-  'doctor_name': { align: 'left', format: null },
-  'doctor_crm': { align: 'center', format: null },
-  'doctor_email': { align: 'left', format: null },
-  'exam_date': { align: 'center', format: formatDate },
-  'exam_type_limits': { align: 'center', format: null },
-  'exam_result': { align: 'center', format: null },
-  'exam_token_result': { align: 'center', format: null },
+  name:              { align:   'left', format: null },
+  citizen_id_number: { align: 'center', format: null },
+  birth_date:        { align: 'center', format: formatDate },
+  crm:               { align: 'center', format: null },
+  email:             { align:   'left', format: null },
+  date:              { align: 'center', format: formatDate },
+  type:              { align: 'center', format: null },
+  limits:            { align: 'center', format: null },
+  result:            { align: 'center', format: null },
+  result_token:      { align: 'center', format: null },
+}
+
+function generateCell(rowData, field, rowsCount=1) {
+  const cell = document.createElement('td')
+
+  cell.setAttribute('rowspan', rowsCount)
+  cell.classList.add(`text-${fieldsData[field].align}`)
+  cell.innerText = fieldsData[field].format
+    ? fieldsData[field].format(rowData[field])
+    : rowData[field]
+
+  return cell
+}
+
+function generateCells(rowData, fields, rowsCount=1) {
+  return fields.map(field => generateCell(rowData, field, rowsCount))
+}
+
+function generateRow(bgColorClass, cells) {
+  const row = document.createElement('tr')
+  row.classList.add('info', bgColorClass)
+  row.append(...cells)
+  return row
 }
 
 function populateTable(data) {
@@ -19,28 +46,27 @@ function populateTable(data) {
 
   tableBody.innerHTML = ''
   data.forEach((rowData, index) => {
-    const tableRow = document.createElement('tr')
-    const rowCells = Object.keys(fieldsData).map((field) => {
-      const cell = document.createElement('td')
+    const rowsCount = rowData.tests.length
 
-      cell.classList.add('info')
-      cell.classList.add(`text-${fieldsData[field].align}`)
-      cell.classList.add(index % 2 ? 'odd' : 'even')
-      cell.innerText = fieldsData[field].format
-        ? fieldsData[field].format(rowData[field])
-        : rowData[field]
+    const patientCells = generateCells(rowData.patient, patientFields, rowsCount)
+    const doctorCells = generateCells(rowData.doctor, doctorFields, rowsCount)
+    const [dateCell, tokenCell] = generateCells(rowData, ['date', 'result_token'], rowsCount)
+    const testsCells = rowData.tests.map(testData => generateCells(testData, testFields))
 
-      return cell
-    })
+    const bgColorClass = index % 2 ? 'odd' : 'even'
+    const firstTestCells = testsCells.shift()
+    const tableRows = [
+      generateRow(bgColorClass, [...patientCells, ...doctorCells, dateCell, ...firstTestCells, tokenCell]),
+      ...testsCells.map(testCells => generateRow(bgColorClass, testCells))
+    ]
 
-    tableRow.append(...rowCells)
-    tableBody.appendChild(tableRow)
+    tableBody.append(...tableRows)
   })
 }
 
 
 function loadTestsAndPopulateTable() {
-  fetch('/api/v1/tests')
+  fetch('/api/v2/tests')
     .then(async (resp) => {
       const exams = await resp.json()
 
--- src/spec/system/home/test_user_visit_home_page_spec.rb
+++ src/spec/system/home/test_user_visit_home_page_spec.rb
@@ -6,8 +6,8 @@ describe 'User visits the home page', type: :feature do
     visit "http://127.0.0.1:#{ENV['API_PORT']}"
 
     expect(page).to have_content 'Lista de Exames Médicos'
-    expect(page).to have_selector 'td[colspan="10"]'
-
+    expect(page).to have_selector 'td[colspan="11"]'
+    expect(page).to have_content 'Nenhum registro para ser exibido!'
   end
 
   it 'and sees existing exams' do
@@ -16,6 +16,17 @@ describe 'User visits the home page', type: :feature do
 
     visit "http://127.0.0.1:#{ENV['API_PORT']}"
 
-    expect(page).not_to have_selector 'td[colspan="10"]'
+    expect(page).not_to have_selector 'td[colspan="11"]'
+    expect(page).not_to have_content 'Nenhum registro para ser exibido!'
+    expect(page).to have_selector 'td[rowspan="2"]'
+    expect(page).to have_content '048.973.170-88'
+    expect(page).to have_content 'Emilly Batista Neto'
+    expect(page).to have_content 'Maria Luiza Pires'
+    expect(page).to have_content '11/03/2001'
+    expect(page).to have_content '05/08/2021'
+    expect(page).to have_content 'hemácias'
+    expect(page).to have_content 'leucócitos'
+    expect(page).to have_content '45-52'
+    expect(page).to have_content '9-61'
   end
 end
