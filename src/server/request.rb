b9e20ea788ba6ae750055ecbaac8a77fd792b2fc
Rosemilson <rozbr96@gmail.com>
2024-03-11 05:18:24 -0300

    Implement multipart upload

--- src/server/request.rb
+++ src/server/request.rb
@@ -2,28 +2,27 @@
 require 'json'
 
 class Request
-  attr_reader :method, :path, :headers, :data, :client, :params
+  attr_reader :method, :path, :headers, :data, :client, :params, :file
 
   def initialize request_text:, client:
     @params = {}
     @client = client
 
-    headers_text, body_text = request_text.split /\r?\n\r?\n/
-    headers_text = headers_text.split /\r?\n/
+    headers_text, body_text = request_text.split(/\r?\n\r?\n/, 2)
+    headers_text = headers_text.split(/\r?\n/)
 
     @method, @path, _ = headers_text.shift.split
     @method.downcase!
 
-    @headers = headers_text.reduce({}) do |headers, line|
-      key, value = line.split ': '
-      headers.update key.downcase => value
-    end
+    @headers = parsed_headers headers_text
 
-    @data = case @headers['content-type']
-    when 'application/json'
-      JSON.parse(%(#{body_text}))
+    case @headers['content-type']
+    when /application\/json/
+      @data = JSON.parse(%(#{body_text}))
+    when /multipart\/form-data/
+      parse_multipart_data body_text, @headers['content-type']
     else
-      body_text
+      @data = body_text
     end
   end
 
@@ -31,5 +30,50 @@ class Request
     return if match_data.nil?
 
     @params = match_data.named_captures
+  end
+
+  private
+
+  def parsed_headers headers_text
+    headers_text = headers_text.strip.split(/\r?\n/) if headers_text.kind_of? String
+    headers_text.reduce({}) do |headers, line|
+      key, value = line.split ': ', 2
+      headers.update key.downcase => value
+    end
+  end
+
+  def parse_multipart_data multipart_data_text, content_type
+    @data = Hash.new { |hash, key| hash[key] = [] }
+
+    boundary = /boundary=(.*)(;|$)/.match(content_type).captures.first
+    boundary = "--#{boundary}"
+
+    regex = Regexp.new "#{boundary}(.*)#{boundary}--", Regexp::MULTILINE
+
+    multipart_part_parts = multipart_data_text.match(regex).captures.first.split boundary
+    multipart_part_parts.each do |multipart_part|
+      headers, content = multipart_part.split(/\r?\n\r?\n/, 2)
+
+      headers = parsed_headers headers
+      content = content.strip
+
+      disposition = headers['content-disposition']
+      if headers['content-type']
+        filename = disposition.match(/filename="(.*)"($|;)/).captures.first
+        @file = Tempfile.new filename
+        @file.write content
+        @file.rewind
+      else
+        varname = disposition.match(/name="(.*)"($|;)/).captures.first
+
+        value = begin
+          JSON.parse content
+        rescue
+          content
+        end
+
+        @data[varname] << value
+      end
+    end
   end
 end
--- src/spec/server/test_request_spec.rb
+++ src/spec/server/test_request_spec.rb
@@ -23,6 +23,28 @@ describe Request do
         expect(request.data['gender']).to eq 'F'
         expect(request.data['height_in_cm']).to eq 169
       end
+
+      it 'parses multipart forms successfully' do
+        request_text = read_file_from_support 'post_upload_request.txt'
+        csv_raw_data = read_file_from_support 'tests_data.csv'
+
+        request = Request.new request_text:, client: nil
+
+        expect(request.method).to eq 'post'
+        expect(request.path).to eq '/upload'
+
+        expect(request.headers['content-length']).to eq '835'
+        expect(request.headers['content-type']).to match 'multipart/form-data'
+
+        expect(request.file).not_to be_nil
+        expect(request.file.path).to match 'tests_data.csv'
+        expect(request.file.read).to eq csv_raw_data
+
+        expect(request.data.kind_of? Hash).to be true
+        expect(request.data['fields']).to eq ['cpf', 'nome paciente']
+        expect(request.data['count']).to eq [2]
+        expect(request.data['overwrite']).to eq [true]
+      end
     end
   end
 end
